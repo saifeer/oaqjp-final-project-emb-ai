fdd398fb9cd1a07a457453285da8d17fe17f084e
saifeer <saifee.ratlamwala@gmail.com>
2026-01-16 23:35:41 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,34 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
+    # URL of the emotion detection service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Custom header specifying the model ID for the emotion detection service
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Constructing the request payload in the expected format
     myobj = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    
+    # Sending a POST request to the emotion detection API
+    response = requests.post(url, json = myobj, headers=header)
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extracting required set of emotions from the response
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    #Dictionary of emotions
+    emotions = {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
+
+    #Extract dominant emotion and add to dictionary
+    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    
+    # Returning a dictionary containing emotion detection results
+    return emotions
